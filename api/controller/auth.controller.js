4d90e016d7564c92d81d03ee995154f4f9b057b4
striver <raavisourav@gmail.com>
2023-12-11 13:23:51 +0530

    added middleware fucn for errorHandling

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     //console.log(req.body);
     const {  username, email, password } = req.body;
     
@@ -13,6 +13,7 @@ export const signup = async (req, res) => {
     await newUser.save()
     res.status(200).json({ message: "User created succesfully "});}
     catch(error){
-       res.status(500).json({ message: "user already exists" });
+    //    res.status(500).json({ message: "user already exists" });
+       next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -6,8 +6,6 @@ import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-
-
 mongoose.connect(process.env.MONGO)
         .then(()=>{
             console.log('connected to mongoDb');
@@ -26,3 +24,13 @@ app.listen(3000, ()=> {
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
