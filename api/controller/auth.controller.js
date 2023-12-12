8bb3b5b2a686589981531fac4693cb26b343249f
striver <raavisourav@gmail.com>
2023-12-12 13:18:20 +0530

    completed signup page feat

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -17,3 +17,7 @@ export const signup = async (req, res, next) => {
        next(error);
     }
 };
+
+export const signin = async () => {
+    
+}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
-import SignOut from "./pages/SignOut"
+import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import Header from './components/Header'
@@ -14,7 +14,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/signin" element={<SignIn/>}/>
-      <Route path="/signout" element={<SignOut/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
       <Route path="/profile" element={<Profile/>}/>
       <Route path="/about" element={<About/>}/>
     </Routes>
