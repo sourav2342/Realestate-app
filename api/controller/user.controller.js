140a382e32726faa5ebac03960887176f01016b9
striver <raavisourav@gmail.com>
2023-12-14 14:46:55 +0530

    create user update api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,5 +1,34 @@
+import User from "../models/user.models.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res)=> {
     res.json({
         message: "Api is working",
     });
 };
+
+export const updateUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You can upd you account"))
+    try {
+        if(req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updateUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                avatar: req.body.avatar,
+            },
+          }, { new: true }
+       );
+
+       const { password, ...rest } = updateUser._doc;
+
+
+    } catch (error) {
+        next(error)
+    }
+}
