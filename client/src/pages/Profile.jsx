07404d34bea03848c130d13c3b495aa8aa6dffb1
striver <raavisourav@gmail.com>
2023-12-30 12:21:10 +0530

    added delete func to user Listings

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -135,7 +135,26 @@ export default function Profile() {
       }
   };
 
+ const handleListingDelete = async (listingId) => {
+    
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await res.json();
 
+      if (data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev) => prev.filter((listing) => listing._id !== listingId));
+
+    } catch (error) {
+      console.log(error.message);
+    }
+ }
 
   return (
     <div className="p-3 max-w-lg mx-auto gap-4">
@@ -174,10 +193,10 @@ export default function Profile() {
               <p>{listing.name}</p>
              </Link>
              <div className="flex flex-col ">
-                <button className="text-red-700 uppercase">
+                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">
                   Delete
                 </button>
-                <button className="text-red-700 uppercase">
+                <button className="text-green-700 uppercase">
                   Edit
                 </button>
              </div>
