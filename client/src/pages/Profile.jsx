20c8adbbeeeb1bfe8af3786cd95ba97eb0980cf2
striver <raavisourav@gmail.com>
2023-12-31 10:45:08 +0530

    added update listing route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -156,6 +156,25 @@ export default function Profile() {
     }
  }
 
+ const handleListingUpdate = async (listingId) => {
+     
+   try {
+     const res = await fetch(`/api/listing/update/${listingId}`, {
+       method: 'UPDATE',
+     });
+     const data = await res.json();
+
+      if (data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListing(data);
+
+   } catch(error) {
+    console.log(error.message);
+   }
+ }
+
   return (
     <div className="p-3 max-w-lg mx-auto gap-4">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -196,7 +215,7 @@ export default function Profile() {
                 <button onClick={() => handleListingDelete(listing._id)} className="text-red-700 uppercase">
                   Delete
                 </button>
-                <button className="text-green-700 uppercase">
+                <button onClick={() => handleListingUpdate(listing._id)} className="text-green-700 uppercase">
                   Edit
                 </button>
              </div>
