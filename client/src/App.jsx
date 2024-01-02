d121bc4324a34815faea6cf0349a25f4b756cd08
striver <raavisourav@gmail.com>
2024-01-02 10:17:47 +0530

    added img slider to liatings page using swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
       <Route path="/signin" element={<SignIn/>}/>
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/listings/:listingId" element={<Listing/>}/>
       <Route  element={<PrivateRoute/>}>
         <Route path="/profile" element={<Profile/>}/>   
         <Route path="/create-listing" element={<CreateListing/>}></Route>
