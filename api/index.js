4e6f3a834213a777a5b9ef93606508ab50457b92
striver <raavisourav@gmail.com>
2024-01-16 12:31:49 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.routes.js";
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -15,6 +15,8 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => {
     console.log(err);
 });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -29,6 +31,12 @@ app.listen(3000, ()=> {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
