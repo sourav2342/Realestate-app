73ecab2ff2a4faa10449882fb50f34c88bec0848
striver <raavisourav@gmail.com>
2024-01-16 12:12:08 +0530

    add show more listings functinality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -58,6 +58,11 @@ export default function Search() {
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length > 8){
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
             
@@ -106,6 +111,20 @@ export default function Search() {
         const searchQuery = urlParams.toString()
         navigate(`/search?${searchQuery}`);
 
+    }
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if(data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     }
 
   return (
@@ -197,6 +216,11 @@ export default function Search() {
                         <ListingItem key={listing._id} listing={listing}/>
                     ))
                 }
+                {showMore && (
+                    <button onClick={onShowMoreClick} className='bg-green-700 hover:underline p-7 text-center w-full'>
+                        show more
+                    </button>
+                )}
             </div>
         </div>
     </div>
